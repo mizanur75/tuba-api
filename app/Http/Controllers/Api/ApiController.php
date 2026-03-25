17f01953fb6503b951a6d9e1579ad0bb0082509b
Mizanur Rahman <36396771+mizanur75@users.noreply.github.com>
2026-03-25 08:03:56 +0600

    about api created

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\About;
 use App\Models\Video;
 use Illuminate\Http\Request;
 
@@ -17,36 +18,10 @@ class ApiController extends Controller
 
         return $data;
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function about()
     {
-        //
-    }
+        $data = About::where('status', 1)->get();
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return $data;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::get('videos', [ApiController::class, 'videos']);
+    Route::get('about', [ApiController::class, 'about']);
 });
