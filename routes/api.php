7a9e8d9578ec075b3dfe2b2be7567ca92b9d4b6d
Mizanur Rahman <36396771+mizanur75@users.noreply.github.com>
2026-03-25 07:16:04 +0600

    videos section api created

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1')->group(function () {
+    Route::get('videos', [ApiController::class, 'videos']);
+});
